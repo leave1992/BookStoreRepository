4cfdd45f627d2157dabaf0d74137bbb1ce616d9c
VITALIK\1440 <1440@ukr.net>
2016-11-19 16:02:56 +0200

    [BooksPage] Added some simple functionality and data

--- BookStore/BookStore/Controllers/BooksController.cs
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class BooksController : Controller
     {
+        private IBooksRepository _booksRepository;
+
         // GET: Books
         public ActionResult Index()
         {
-            return View();
+            var repository = new BooksRepository();
+            _booksRepository = repository;
+            var books =  _booksRepository.GetBooks();
+
+            return View(books);
         }
     }
 }
