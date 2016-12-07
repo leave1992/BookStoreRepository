d64332989804f296d2c07edbb13bf2f673a93a45
VITALIK\1440 <1440@ukr.net>
2016-12-07 15:55:32 +0200

    [MigrationAndAddingBookFunctionality] Added validation, migration and Admin books functionality

--- BookStore/BookStore/Controllers/AdminController.cs
+++ BookStore/BookStore/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +12,13 @@ namespace BookStore.Controllers
     public class AdminController : Controller
     {
         private BooksStoreContext _context = new BooksStoreContext();
-        // GET: Admin
+
+        [HttpGet]
+        public ActionResult AllBooks()
+        {
+            return View(_context.Books.ToList());
+        }
+
         [HttpGet]
         public ActionResult AddBook()
         {
@@ -22,13 +29,71 @@ namespace BookStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult AddBook(Book book)
         {
-            //if (ModelState.IsValid)
-            //{
-            _context.Books.Add(book);
-            _context.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
                 return RedirectToAction("Index", "Books");
-            //}
-
+            }
+            return View(book);
         }
+
+        [HttpGet]
+        public ActionResult EditBook(string id = null)
+        {
+            int intId;
+            int.TryParse(id, out intId);
+            Book book = _context.Books.Find(intId);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(book).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Books");
+            }
+            return View(book);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteBook(string id = null)
+        {
+            int intId;
+            int.TryParse(id, out intId);
+            Book book = _context.Books.Find(intId);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            int intId;
+            int.TryParse(id, out intId);
+            Book book = _context.Books.Find(intId);
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Books");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+
     }
 }
